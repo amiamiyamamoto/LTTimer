aa2b0c99e55a658a4bed0d263907523139d2a309
AMI <a_yamamoto@innovation.co.jp>
2018-03-01 21:18:56 +0900

    Initial Commit

---
+++ Timer2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer2
+//
+//  Created by s247 on 2018/03/01.
+//  Copyright © 2018年 ami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
